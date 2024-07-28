90da4efe848f025921f28502d6935f188a3f7bde
aniruddhku <aniruddhkulkarni.kulkarni@gmail.com>
2024-07-28 23:28:26 +0100

    Demo App

---
+++ src/main/java/com/ak/app/ak/AkDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ak.app.ak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak/app/ak/AkDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.app.ak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
